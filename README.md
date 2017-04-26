0f119cfe6a4adc17c254515bc109ce251bea20d8
Fabricio Bezerra <demonstro003@gmail.com>
2017-04-26 18:18:46 -0300

    Ping pong and die

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Dawnbot Discordbot
+
+## Atention
+
+If you are not running with **NODE_ENV = "production"** you must have a *config.js* file with your project with a *discord bot token*, a *random message* and the bot admin *info*.
+
+If you are running with **NODE_ENV = "production"** you must use a .env file including a *discord bot token*, a *random message* and the bot admin *info*. (env files are to be used mostly with heroku (In this project), you may need to work arround if you want to use env without).
+
+## TODOS:
+* Develop the admin user system
--- bot.js
+++ bot.js
@@ -1,1 +1,30 @@
-console.log('I\'m a bot :>');
+var config;
+const Discord = require("discord.js");
+
+if (process.env.NODE_ENV != "production") {
+    var config = require('./config.js');
+}
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    } else if (msg.content === 'die') {
+        msg.reply('Ded');
+        console.log('Bot is dying');
+        killClient();
+    }
+});
+
+function killClient() {
+    client.destroy();
+}
+
+var promise = client.login(config.token || process.env.token);
+
+console.log(promise);
